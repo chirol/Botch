6d7cd3dce4a8e955c17ea829899792954af6a503
chirol <gamingdogcrl@gmail.com>
2019-10-14 10:45:13 +0900

    10/14

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*)e!syd_!b18lku=z8oo_ig^asn7c($%qjv$d_-5!%++t3_9d0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
