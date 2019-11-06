1ae5037ff2a6d0e7f3cdd3ac35219f60f431b339
chirol <gamingdogcrl@gmail.com>
2019-11-06 19:18:18 +0900

    add models.py recruiment prefecture

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Recruitment(models.Model):
+    # 募集に必要な情報。
+    game = models.CharField(
+        verbose_name='プレイしたいボードゲームやジャンル',
+        max_length=100,
+        default=''
+        blank=False
+    )
+    area = models.ManyToManyField(
+        Prefecture,
+        verbose_name='遊びたい地域（県）'
+        blank=False
+    )
+    place = models.CharField(
+        verbose_name='遊びたいカフェや場所',
+        max_length=100,
+    )
+    date = models.DateField(
+        verbose_name='遊びたい日時',
+    )
+    comment = models.TextField(
+        verbose_name='その他コメントなど',
+        max_length='400',
+    )
+
+class Prefecture(models.Model):
+    """
+    遊びたいエリア指定のための県クラス
+    """
+    prefecture = models.CharField(
+        verbose_name='遊びたい地域',
+    )
+    
+    def __str__(self):
+        return self.prefecture
