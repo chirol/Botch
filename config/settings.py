b38c063e77a30f4cd119510ab46077a445fc242e
chirol <gamingdogcrl@gmail.com>
2019-12-02 20:35:12 +0900

    pipeline

--- config/settings.py
+++ config/settings.py
@@ -146,8 +146,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 )
 
 # SOCIAL_AUTH_FIELDS_STORED_IN_SESSION = ['session_user']
