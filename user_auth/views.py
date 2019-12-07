66a87bf491592cc6a69869ec04e83ada1e367dda
chirol <gamingdogcrl@gmail.com>
2019-12-07 18:41:57 +0900

    recruiment_detail.html Redruitment for recruitment

--- user_auth/views.py
+++ user_auth/views.py
@@ -30,7 +30,7 @@ class RecruitCreateView(CreateView):
 
     def get_initial(self):
         self.user = UserSocialAuth.objects.get(user_id=self.request.user.id)
-        self.initial_form = {'username': self.user, 'userid': self.user.id} #twitter情報からユーザid（一意)をフォームの初期値に
+        self.initial_form = {'username': self.user, 'userid': self.user.id}
         return self.initial_form
 
 """
@@ -71,7 +71,6 @@ class RecruitmentUpdateView(UpdateView):
     def user_auth(request):
         self.user = UserSocialAuth.objects.get(user_id=self.request.user.id)
         return render(request, 'user_auth/create_form.html', {'user': self.user})
-
 
 
 class RecruitmentListView(ListView):
