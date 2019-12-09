55ace9383a3241fefcc1cdc6eae150f741f109b9
chirol <gamingdogcrl@gmail.com>
2019-12-09 10:10:04 +0900

    self, request

--- user_auth/views.py
+++ user_auth/views.py
@@ -68,7 +68,7 @@ class RecruitmentUpdateView(UpdateView):
     model = Recruitment
     template_name = "user_auth/recruitment_update.html"
 
-    def user_auth(request):
+    def user_auth(self, request):
         self.user = UserSocialAuth.objects.get(user_id=self.request.user.id)
         return render(request, 'user_auth/create_form.html', {'user': self.user})
 
