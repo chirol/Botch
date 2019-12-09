a3e61605c441bdd380fd269b7bb15b9f271c32df
chirol <gamingdogcrl@gmail.com>
2019-12-09 09:59:18 +0900

    comment out listview user_auth

--- user_auth/views.py
+++ user_auth/views.py
@@ -80,9 +80,11 @@ class RecruitmentListView(ListView):
     model = Recruitment
     template_name = "user_auth/top.html"
 
+    """
     def user_auth(request):
         self.user = UserSocialAuth.objects.get(user_id=self.request.user.id)
         return render(request, 'user_auth/create_form.html', {'user': self.user})
+    """
 
 """
 class RecruitmentDeleteView(DeleteView):
